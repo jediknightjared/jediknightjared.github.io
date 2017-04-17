68136bea188752d97d87b0c4fe63f5376bc561b4
jediknightjared <jaredthebillionaire@outlook.com>
2017-04-17 10:49:14 -0600

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,5 @@
+this.addEventListener("load", function() {
+	var nav = document.getElementById("nav");
+	nav.innerHTML= '<table><tr><td><a href="/"><img src="Images/Logo.png" id="logo"></a></td><td><a href="/games.html">Games</a></td></tr></table>';
+	
+});
