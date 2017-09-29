837b5de4368bab74a4b179eb2cc6cf251064b542
jediknightjared <jaredthebillionaire@outlook.com>
2017-09-29 15:34:27 -0600

    Update nav.js

--- nav.js
+++ nav.js
@@ -1,5 +1,5 @@
 this.addEventListener("load", function() {
 	var nav = document.getElementById("nav");
-	nav.innerHTML= '<table><tr><td><a href="/"><img src="Images/Logo.png" id="logo"></a></td><td><a href="/games.html">Games</a></td></tr></table>';
+	nav.innerHTML= '<table><tr><td><a href="/"><img src="Images/Logo.png" id="logo"></a></td><td><a href="/games.html">Games</a></td><td><a href="TO-DO_list.html">Todo list</a></td></tr></table>';
 	
 });
